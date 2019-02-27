3e50f8c4cea2462276870530073cfc0db1d9c639
kalaikannanJK <47594774+kalaikannanJK@users.noreply.github.com>
2019-02-27 21:48:29 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int i=0;
+void main()
+{
+ int a;
+ scanf("%d",&a);
+ if(a>0)
+ {
+  while(a>9)
+  {
+    n=n/10;
+    i++;
+  }
+ printf("total digit %d",i+1);
+ }
+ else
+  {
+   printf("No should b greater than 0");
+  }
+  getch();
+}
